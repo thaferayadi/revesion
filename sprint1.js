5546551eff1a83a5809f8d984c2c933f230e3eea
Ayadi Mouhamed Thafer <thaferayadi@gmail.com>
2023-12-30 17:21:23 +0100

    done

---
+++ sprint1.js
@@ -0,0 +1,39 @@
+//exercice advanced
+//Compiled languages are translated into machine code before execution, resulting in faster performance. 
+// Interpreted languages are executed line by line, with code translated at runtime, 
+//  making them more flexible but potentially slower. 
+  // Some languages, like Java, use a combination of both, employing a bytecode compilation step for portability.
+  //High-level languages provide abstraction from hardware details, enabling easier code development compared to low-level languages. They are more human-readable and portable across different platforms.
+
+
+//Weakly typed languages allow flexibility with variable types, often leading to implicit type conversions, which can result in unexpected behavior.
+//v8 is an open source javascript engine develloped by google.b8 is responsible for excuting code javascript into ther browser.
+
+
+//An engine is a software component that executes tasks or processes, often associated with interpreting or processing specific types of data or instructions.
+
+
+//JavaScript is a widely-used, high-level, interpreted programming language primarily known for its role in web development, enabling dynamic and interactive content on websites. 
+
+//JavaScript is chosen for its versatility, as it runs in web browsers, allowing for dynamic and interactive web pages.
+// Its ease of learning, large developer community,
+ //and cross-platform compatibility contribute to its widespread adoption.
+
+
+//A functional-based language emphasizes a functional programming paradigm, treating computation as the evaluation of mathematical functions and promoting immutability and higher-order functions for expressive and concise code.
+
+//An object-oriented language organizes code around objects, which encapsulate data and behavior. It promotes concepts like inheritance, encapsulation, and polymorphism to structure and model software systems.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
