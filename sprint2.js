bbf5c4e5119c66e46374db66eced4400d9d664fe
Ayadi Mouhamed Thafer <thaferayadi@gmail.com>
2023-12-30 17:36:23 +0100

    done

---
+++ sprint2.js
@@ -0,0 +1,51 @@
+function myFirstFunction(){
+    
+ } 
+  myFirstFunction()
+  //
+  function myFirstFunction(){
+    console.log("hello")
+} 
+myFirstFunction()
+//
+ function returnFive() {
+    console.log(5);
+}
+
+returnFive();  
+
+function helloWorldAgain() {
+    return "Hello World Again";
+}
+// 
+function returnParameter(n) {
+    return n;
+ }
+ //
+ function add(number1, number2) {
+    return number1 + number2;
+ }
+
+ function sub(number1, number2) {
+    return number1 - number2;
+ }
+ function multiplication(number1, number2) {
+    return number1 * number2;
+ }
+ function stringLength(str) {
+    return str.length;
+  }
+  function concatTwoStrings(string1, string2) {
+    return string1 + string2;
+  }
+  function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+  }
+  
+  
+
+
+
+ 
+
+ 
